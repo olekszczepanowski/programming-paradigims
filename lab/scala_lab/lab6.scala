940c388b7c9f7cc8d6c667262930abe8e70f71be
olekszczepanowski <120830093+olekszczepanowski@users.noreply.github.com>
2023-12-01 18:22:01 +0100

    added task1

--- lab/scala_lab/lab6.scala
+++ lab/scala_lab/lab6.scala
@@ -1,15 +1,45 @@
 @main
 def main(): Unit = {
-  val lista = LazyList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-  val skipList = skipM(lista,4)
-  println(skipList.toList)
+  val lazyList1 = LazyList(5,6,3,2,1)
+  val lazyList2= LazyList(1,2,3,4,5,6,7,8)
+  val lazyList3= LazyList()
+  val test1 = choose(lazyList1,2,1)
+  val test2 = choose(lazyList2,3,2)
+  val test3 = choose(lazyList3,2,3)
+
+  println(test1.toList)
+  println(test2.toList)
+  println(test3.toList)
+  // Wyrzucenie wyjatku dla zlej wartosci n
+  //  val test4 = choose(lazyList1,0,3)
+  //  println(test4.toList)
+
+  // Wyrzucenie wyjatku dla zlej wartosci m
+  //val test5 = choose(lazyList1, 2, -3)
+  //  println(test4.toList)
+}
+class WrongValue extends Exception
+def choose(list: LazyList[Int], n: Int, m: Int): LazyList[Int] = {
+  if (n < 1)  throw new WrongValue
+  else if (m < 0) throw new WrongValue
+  else {
+    val skippedList = skip(list, m-1)
+    def chooseHelper(tmpList: LazyList[Int], tmp: Int): LazyList[Int] = {
+      tmpList match {
+        case LazyList() => LazyList.empty
+        case h #:: tail =>
+          if (tmp % n == 0) h #:: chooseHelper(tail, tmp + 1)
+          else chooseHelper(tail, tmp + 1)
+      }
+    }
+    chooseHelper(skippedList, 0)
+  }
 }
 
-
-def skipM(list: LazyList[Int], count: Int): LazyList[Int] = {
-  if (count <= 0) list
+def skip(list: LazyList[Int], m: Int): LazyList[Int] = {
+  if (m <= 0) list
   else list match {
     case LazyList() => LazyList.empty // Obsługa pustej listy
-    case _ #:: tail => skipM(tail, count - 1)
+    case _ #:: tail => skip(tail, m - 1)
   }
 }
