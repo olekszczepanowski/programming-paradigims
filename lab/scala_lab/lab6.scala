0403f56d5c2f72837f4311db78ae7aa111ce069e
olekszczepanowski <120830093+olekszczepanowski@users.noreply.github.com>
2023-12-04 09:54:53 +0100

    changed to use Stream instead of LazyList

--- lab/scala_lab/lab6.scala
+++ lab/scala_lab/lab6.scala
@@ -1,8 +1,8 @@
 @main
 def main(): Unit = {
-  val lazyList1 = LazyList(5,6,3,2,1)
-  val lazyList2= LazyList(1,2,3,4,5,6,7,8)
-  val lazyList3= LazyList()
+  val lazyList1 = 5 #:: 6 #:: 3 #:: 2 #:: 1 #:: Stream.empty
+  val lazyList2=  1 #:: 2 #:: 3 #:: 4 #:: 5 #:: 6 #:: 7 #:: 8 #:: Stream.empty
+  val lazyList3= Stream.empty
   val test1 = choose(lazyList1,2,1)
   val test2 = choose(lazyList2,3,2)
   val test3 = choose(lazyList3,2,3)
@@ -10,23 +10,23 @@ def main(): Unit = {
   println(test1.toList)
   println(test2.toList)
   println(test3.toList)
-  // Wyrzucenie wyjatku dla zlej wartosci n
-  //  val test4 = choose(lazyList1,0,3)
-  //  println(test4.toList)
+//   Wyrzucenie wyjatku dla zlej wartosci n
+//    val test4 = choose(lazyList1,0,3)
+//    println(test4.toList)
 
   // Wyrzucenie wyjatku dla zlej wartosci m
   //val test5 = choose(lazyList1, 2, -3)
   //  println(test4.toList)
 }
 class WrongValue extends Exception
-def choose(list: LazyList[Int], n: Int, m: Int): LazyList[Int] = {
+def choose(list: Stream[Int], n: Int, m: Int): Stream[Int] = {
   if (n < 1)  throw new WrongValue
   else if (m < 0) throw new WrongValue
   else {
     val skippedList = skip(list, m-1)
-    def chooseHelper(tmpList: LazyList[Int], tmp: Int): LazyList[Int] = {
+    def chooseHelper(tmpList: Stream[Int], tmp: Int): Stream[Int] = {
       tmpList match {
-        case LazyList() => LazyList.empty
+        case Stream() => Stream.empty
         case h #:: tail =>
           if (tmp % n == 0) h #:: chooseHelper(tail, tmp + 1)
           else chooseHelper(tail, tmp + 1)
@@ -36,10 +36,10 @@ def choose(list: LazyList[Int], n: Int, m: Int): LazyList[Int] = {
   }
 }
 
-def skip(list: LazyList[Int], m: Int): LazyList[Int] = {
+def skip(list: Stream[Int], m: Int): Stream[Int] = {
   if (m <= 0) list
   else list match {
-    case LazyList() => LazyList.empty // Obsługa pustej listy
+    case Stream() => Stream.empty
     case _ #:: tail => skip(tail, m - 1)
   }
 }
