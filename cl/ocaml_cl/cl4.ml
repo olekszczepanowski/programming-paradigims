fdc90d1c39bc987713869ce0a81d47ab1b46883b
olekszczepanowski <120830093+olekszczepanowski@users.noreply.github.com>
2023-12-07 19:04:33 +0100

    added graphs

--- cl/ocaml_cl/cl4.ml
+++ cl/ocaml_cl/cl4.ml
@@ -73,3 +73,25 @@ let outer_path bt =
 
 outer_path tt;;
   
+
+(* zad5 *)
+type 'a graph = Graph of ('a -> 'a list)
+
+let g = Graph
+(function
+0 -> [3]
+| 1 -> [0;2;4]
+| 2 -> [1]
+| 3 -> []
+| 4 -> [0;2]
+| n -> failwith ("Graph g: node "^string_of_int n^" doesn't exist")
+);;
+let breadthSearch (Graph succ) startNode =
+  let rec search visited queue =
+  match queue with
+  [] -> []
+  | h::t -> if List.mem h visited then search visited t
+  else h :: search (h :: visited) (t @ succ h)
+  in search [] [startNode];;
+
+  breadthSearch g 4;; 
