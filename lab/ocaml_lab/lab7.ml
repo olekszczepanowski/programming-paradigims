1bafe3fd8030aeae038219f1ccffe5d1f0860cc1
olekszczepanowski <120830093+olekszczepanowski@users.noreply.github.com>
2023-12-08 16:05:06 +0100

    lab7 done

--- lab/ocaml_lab/lab7.ml
+++ lab/ocaml_lab/lab7.ml
@@ -40,16 +40,53 @@ let rec ltake = function
 |(n,LElement(x,xf)) -> x::ltake(n-1,xf())
 ;;
 
-let rec powiel list =
-  let rec powtorz n elem =
-    if n <= 0 then Koniec
-    else Element (elem, fun () -> powtorz (n - 1) elem)
-  in
-  match list with
-  | Koniec -> Koniec
-  | Element (hd, tl) -> powtorz hd hd @ powiel tl ()
+let rec append list1 list2 = 
+  match list1 with
+  | Koniec -> list2 
+  | Element (head, tail) -> Element (head, append tail list2)
 ;;
 
-let list1 = Element(1, Element(2, Element(3, Koniec)));;
-powiel list1;;
+let rec replicateElem elem acc = 
+  if elem < 0 then failwith "Blad. Lista ma ujemne wartosci"
+  else
+    match acc with
+    | 0 -> Koniec
+    | _ -> Element(elem, replicateElem elem (acc-1))
+;;
+
+let rec replicate list = 
+  match list with
+  |Element (hd,tl) -> append (replicateElem hd hd) (replicate tl)
+  |_ -> Koniec
+;;
+
+let list = (Element(1, Element(2, Element (3, Koniec))));;
+replicate list;;
+
+(* lazylist *)
+
+let rec lappend list1 list2 = 
+  match list1 with
+  | LKoniec -> list2 
+  | LElement (head, tail) -> LElement (head, function () -> lappend (tail()) list2)
+;;
+
+let rec lreplicateElem elem acc = 
+  if elem < 0 then failwith "Blad. Lista ma ujemne wartosci"
+  else
+    match acc with
+    | 0 -> LKoniec
+    | _ -> LElement(elem, function () -> lreplicateElem elem (acc-1))
+;;
+
+let rec lreplicate list = 
+  match list with
+  |LElement (hd,tl) -> lappend (lreplicateElem hd hd) (lreplicate (tl()))
+  |_ -> LKoniec
+;;
+
+let ll1 = LElement(1, function () -> LElement(2, function () -> LElement(3, function () -> LKoniec)));;
+let test = lreplicate ll1;;
+
+ltake (6, (test));;
 
