025b96e4938fd48cdc401d17b82c73807a5ec36e
olekszczepanowski <120830093+olekszczepanowski@users.noreply.github.com>
2024-01-12 13:05:49 +0100

    cl 6 and 8

---
+++ cl/java_cl/cl6/src/IsEqual.java
@@ -0,0 +1,9 @@
+public class IsEqual{
+    static boolean isEqual1(int m, int n){return m == n;}
+    static boolean isEqual2(Integer m, Integer n){return m == n;}
+    public static void main(String[] args){
+        System.out.println(isEqual1(500,500));
+        System.out.println(isEqual2(500,500));
+        // int to prymityw a Integer to objekt, a objekty porownujemy uzywajac equals
+    }
+}
---
+++ cl/java_cl/cl8/src/Test.java
@@ -0,0 +1,28 @@
+public class Test {
+    int zawartość = 0;
+    static void argNiemodyfikowalny(final Test zmienna) {
+        zmienna.zawartość = 1;
+//        zmienna = null;
+    }
+    static void argModyfikowalny(Test zmienna) {
+        zmienna.zawartość = 1;
+        zmienna = null;
+    }
+    public static void main(String[] args) {
+        Test modyfikowalna = new Test();
+        final Test niemodyfikowalna = new Test();
+// tutaj wstaw instrukcje
+        argNiemodyfikowalny(modyfikowalna);
+        System.out.println(modyfikowalna.zawartość);
+
+        argNiemodyfikowalny(niemodyfikowalna);
+        System.out.println(niemodyfikowalna.zawartość);
+
+        argModyfikowalny(modyfikowalna);
+        System.out.println(modyfikowalna.zawartość);
+
+        argModyfikowalny(niemodyfikowalna);
+        System.out.println(niemodyfikowalna.zawartość);
+
+    }
+}
